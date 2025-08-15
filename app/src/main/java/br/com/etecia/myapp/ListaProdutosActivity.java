5ad021ca3f6852c5e4dbfd838efa8ce7d5e830e1
ifkimb <kcarolinw0704@gmail.com>
2025-08-15 20:19:40 -0300

    lista de itens

--- app/src/main/java/br/com/etecia/myapp/ListaProdutosActivity.java
+++ app/src/main/java/br/com/etecia/myapp/ListaProdutosActivity.java
@@ -1,12 +1,15 @@
 package br.com.etecia.myapp;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -43,5 +46,13 @@ public class ListaProdutosActivity extends AppCompatActivity {
         lstProdutos.add(new Produtos("notebook", R.drawable.notebook));
 
         idListaProdutos = findViewById(R.id.idListaProdutos);
+
+        //tipo de layout
+        idListaProdutos.setLayoutManager(new GridLayoutManager(getApplicationContext(),2));
+
+        //instanciar o adaptador
+        AdaptadorProdutos adapter = new AdaptadorProdutos(getApplicationContext(),lstProdutos);
+
+        idListaProdutos.setAdapter(adapter);
     }
 }
